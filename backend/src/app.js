2d7d0659356ce3e088a4c87381126298d6f6c8a6
Naveed <abdulnaveedmohammed81@gmail.com>
2024-10-07 08:18:50 +0530

    explore http methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("hello from the server");
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Mohd", lastname: "Naveed" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data Successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully");
 });
 
 app.listen(3002, () => {
