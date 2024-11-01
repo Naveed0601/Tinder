6b2cdcd63e745cbeea5ee4a9fa16f59491768525
Naveed <abdulnaveedmohammed81@gmail.com>
2024-11-01 07:57:22 +0530

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://Naveed:abdulnaveed@cluster.yti3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
-  );
+""  );
 };
 
 module.exports = connectDB;
